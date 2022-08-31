f01afea14667c3c27112659ba62bdba96386d30b
Lucas Salvino <lucassalvino1@gmail.com>
2022-08-31 15:46:11 -0300

    cache

---
+++ app/Utils/ApiCache.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Utils;
+
+use Illuminate\Support\Facades\Cache;
+
+class ApiCache{
+    protected static $cacheTempo = null;
+
+    public static function ObtemTempoCache() {
+        if ( is_null(self::$cacheTempo) ) {
+            self::$cacheTempo = env('CACHE_LIFE_TIME', 30);
+        }
+        return self::$cacheTempo;
+    }
+
+    public static function Existe($chave){
+        return Cache::has($chave);
+    }
+    
+    public static function AddCache($chave, $valor, $fatorTempo = 1){
+        return Cache::put($chave, $valor, (static::ObtemTempoCache() * $fatorTempo));
+    }
+
+    public static function Obtem($chave){
+        return Cache::get($chave);
+    }
+    
+    public static function Remove($chave){
+        Cache::forget($chave);
+    }
+
+    public static function LimpaCahce(){
+        Cache::flush();
+    }
+
+    public static function GeraChaveRequest(Array $filtros){
+        return md5(json_encode($filtros));
+    }
+}

287de7049f203e28d68b94eb8f3e6eeb01221c83
Lucas Salvino <lucassalvino1@gmail.com>
2022-11-29 17:21:57 -0300

    validacao numero telefone

--- app/Utils/Valida.php
+++ app/Utils/Valida.php
@@ -51,4 +51,10 @@ class Valida{
     public static function PadraoValido($valor){
         return true;
     }
+
+    public static function ValidaStringNumeroTelefone($vlr){
+        return preg_match(
+            "%^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$%i",
+            $vlr);
+    }
 }

9d8b2901d7171da89d3d6597b269ee4a5b396b66
Victoria Maia <victoria.bartesaghii@gmail.com>
2024-02-08 17:46:07 -0300

    migrate certo

--- database/migrations/2024_02_08_163443_duvidas.php
+++ database/migrations/2024_02_08_163443_duvidas.php
@@ -15,11 +15,11 @@ class Duvidas extends Migration
     {
         Schema::create('duvidas', function (Blueprint $table){
             $table->uuid('id')->primary();
+            $table->string('titulo', 255);
+            $table->integer('ordem');
+            $table->text('resposta')->nullable();
             $table->softDeletes();
             $table->timestamps();
-            $table->string('titulo', 255);
-            $table->int('ordem');
-            $table->text('resposta')->nullable();
         });
 
     }

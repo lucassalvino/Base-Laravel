ab175dcf2b47899cdc5f617f9c6af85b1e71c5b7
Victoria Maia <victoria.bartesaghii@gmail.com>
2024-02-09 10:21:29 -0300

    validadores

--- app/Models/Duvidas.php
+++ app/Models/Duvidas.php
@@ -8,4 +8,16 @@ Class Duvidas extends BaseModel{
     protected $fillable = [
         'id', 'titulo', 'ordem', 'resposta'
     ];
+    public function GetValidadorCadastro($request){
+        return [
+            'titulo' => ['required', 'max:255'],
+            'ordem' => 'required',
+            'resposta' => 'required'
+        ];
+    }
+    public function GetValidadorAtualizacao($request, $id){
+        $valida = $this->GetValidadorCadastro($request);
+        $valida['titulo'] = ['required', 'max:255'];
+        return $valida; 
+    }
 }

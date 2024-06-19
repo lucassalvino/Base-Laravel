0128934c658b279e980d5f7eab868ea78d4e2440
Victoria Maia <victoria.bartesaghii@gmail.com>
2024-06-19 13:34:35 -0300

    footer iniciado

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,21 @@
-<html>
-<body>
-    </body>
+<footer class="footer-bg">
+    <div class="footer">
+        <img src="{{asset('assets/img/admin/cit_back.svg')}}" alt="">
+        <div class="footer-contatos">
+            <p class="titulo">CONTATO</p>
+            <p class="texto">+55 62 99999-9999</p>
+            <p class="texto">contato@cit.com</p>
+        </div>
+        <div class="footer-informacoes">
+            <p class="titulo">INFORMAÇÕES</p>
+            <p class="texto">HOME</p>
+            <p class="texto">FAQ</p>
+            <p class="texto">SOBRE</p>
+        </div>
+
+    </div>
+</footer>
+</body>
+
+
 </html>

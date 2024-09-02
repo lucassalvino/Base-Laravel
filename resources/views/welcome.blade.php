1482db6e9d6b3c203c74c361ccf84bfb34ba8617
Yan Menezes <pignata.yan@gmail.com>
2024-09-02 10:38:55 -0300

    Revert "Revert "Consumindo API publica omdb Filmes""
    This reverts commit d5ee449c45b3c77b08cc138044f9627e68fcfbf1.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,22 @@
 
 
 @section('content')
-    @include('includes.Componentes.titulo', ['titulo' => ''])
-    @include('includes.Componentes.titulo', ['titulo' => ''])
+    <div class="container my-2">
+        <form class="formValidate formAjax" method="POST" action="{{route('consulta-filme')}}">
+            @csrf
+            <div class="row">
+                <div class="col-md-6">
+                    <label for="titulo">Título:</label>
+                    <input type="text" name="titulo" required class="form-control">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <label for="titulo">Ano do Filme:</label>
+                    <input type="number" name="ano" placeholder="Não é obrigatório" class="form-control">
+                </div>
+            </div>
+            <button class="btn btn-primary mt-2" type="submit"> Buscar</button>
+        </form>
+    </div>
 @stop
